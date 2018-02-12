11071788ab6915763ed01d70be4693c17ab7de4c
segee <segee@maine.edu>
2018-02-12 10:13:28 -0500

    Update rpi_startup.sh
    Need to fix update_tunnels.sh call

--- rpi_startup.sh
+++ rpi_startup.sh
@@ -1,5 +1,6 @@
 # Does all startup for a rpi usb hub (installs usbip and all modules, makes tunnels to guac server, and enables udev rules to bind devices)
 sudo enable_usbip_server.sh
+###UPDATE_TUNNELS TAKES AN ARGUMENT ###
 sudo update_tunnels.sh
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/enable_udevrules.yml --ask-sudo-pass
 echo You need to reboot in order to have some changes
