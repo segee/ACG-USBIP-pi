c396ec68f85ceaa6d7add2a2f8c40e6478a4d39c
segee <segee@maine.edu>
2018-02-12 10:12:17 -0500

    Update enable_udevrules.sh
    Should take a look at how to update rules

--- enable_udevrules.sh
+++ enable_udevrules.sh
@@ -5,3 +5,5 @@ sudo apt-get update
 sudo apt-get install ansible -y
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/enable_udevrules.yml --ask-sudo-pass
 echo A reboot is needed for this change to take effect
+#https://unix.stackexchange.com/questions/39370/how-to-reload-udev-rules-without-reboot
+
