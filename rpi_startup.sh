825fc9b6d7a1d7e726612104ccb18a34198b22f6
segee <segee@maine.edu>
2018-02-28 05:37:39 -0500

    Added full path for the call to enable_usbip_server

--- rpi_startup.sh
+++ rpi_startup.sh
@@ -1,19 +1,14 @@
 # Does all startup for a rpi usb hub (installs usbip and all modules, makes tunnels to guac server, and enables udev rules to bind devices)
-sudo enable_usbip_server.sh
+sudo /usr/local/bin/ACG-USBIP-pi/enable_usbip_server.sh
 if [ $# -eq 0 ] || [ $# == "--help" ] || [ $# == "-h" ]
 then
-    echo Usage: update_tunnels.sh ip where ip is the least significant value of your VMs IP
-    echo Ex: 192.168.0.92
-    echo You would enter update_tunnels.sh 92
+    echo Please run with your Unique ID as the first and only argument
     exit
 fi
 
 if [ $1 -lt 0 ] || [ $1 -gt 255 ]
 then
-    echo Ip must be between 0-255
-    echo Usage: update_tunnels.sh ip where ip is the least significant value of your VMs IP
-    echo Ex: 192.168.0.92
-    echo You would enter update_tunnels.sh 92
+    echo Unique ID must be between 0-255
     exit
 fi
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/update_tunnels.yml --ask-sudo-pass --extra-vars "vm_ip=$1"
