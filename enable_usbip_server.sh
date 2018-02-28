111c3c7fce26d016c8fe0a3ee4aadb4ce75d950a
dparadis21 <dan.paradis21@gmail.com>
2018-02-28 12:58:49 -0500

    Update enable_usbip_server.sh

--- enable_usbip_server.sh
+++ enable_usbip_server.sh
@@ -7,7 +7,7 @@ sudo apt-get update
 sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
-sudo apt-get install ansible -y
+sudo apt-get install ansible usbip -y
 echo "enabling USB IP kernel modules..."
 sudo modprobe usbip-host > /dev/null &
 sudo modprobe usbip-core > /dev/null &
