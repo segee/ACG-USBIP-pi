931fea233ec653b23a5ccc0f046f0e1e0a8e8efb
segee <segee@maine.edu>
2019-02-04 16:32:52 -0500

    can now run update tunnels first

--- update_tunnels.sh
+++ update_tunnels.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+sudo apt-get update
+sudo apt-get install software-properties-common -y
+sudo apt-add-repository ppa:ansible/ansible -y
+sudo apt-get update
+sudo apt-get install ansible -y
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/update_tunnels.yml --ask-sudo-pass --extra-vars "vm_ip=$1"
