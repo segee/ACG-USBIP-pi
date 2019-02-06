5536538ea27f03933862461938d6722db15da743
segee <segee@maine.edu>
2019-02-06 15:44:08 -0500

    change permissions on id_rsa

--- update_tunnels.sh
+++ update_tunnels.sh
@@ -4,4 +4,5 @@ sudo apt-get install software-properties-common -y
 sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
+sudo chmod 400 /usr/local/bin/ACG-USBIP-pi/id_rsa
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/update_tunnels.yml --ask-sudo-pass --extra-vars "vm_ip=$1"
