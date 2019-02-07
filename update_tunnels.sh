368c786f8ad0b1f94991097cb0efa21e034485db
segee <segee@maine.edu>
2019-02-07 17:31:20 -0500

    finally getting ssh working

--- update_tunnels.sh
+++ update_tunnels.sh
@@ -5,4 +5,6 @@ sudo apt-add-repository ppa:ansible/ansible -y
 sudo apt-get update
 sudo apt-get install ansible -y
 sudo chmod 400 /usr/local/bin/ACG-USBIP-pi/id_rsa
+sudo mkdir /.ssh
+sudo ssh-keyscan -H 204.197.1.140 >>/.ssh/known_hosts
 ansible-playbook /usr/local/bin/ACG-USBIP-pi/playbooks/update_tunnels.yml --ask-sudo-pass --extra-vars "vm_ip=$1"
